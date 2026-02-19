3e70ef2dc5cd0a678382d2a7f0236d664e069476
141Taskstalker <mananthaker2505@gmail.com>
2026-02-19 16:13:00 +0530

    Create Constructor.cs

---
+++ Constructor.cs
@@ -0,0 +1,23 @@
+using System;
+
+class Student
+{
+    public string name;
+
+    public Student(string n)
+    {
+        name = n;
+    }
+}
+
+
+class Program
+{
+    static void Main()
+    {
+        
+       
+
+
+    }
+}
