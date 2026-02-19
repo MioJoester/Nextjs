6867090a4096e286a4c23c8a5a606a85a393dfb1
141Taskstalker <mananthaker2505@gmail.com>
2026-02-19 16:07:59 +0530

    Create Method.cs

---
+++ Method.cs
@@ -0,0 +1,20 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+       int result = Multiply(3, 4);
+Console.WriteLine(result);
+
+
+
+    }
+    
+    static int Multiply(int a, int b)
+{
+    return a * b;
+}
+
+}
