455ce9a6117b28d240e18065df74bcb4afa23640
141Taskstalker <mananthaker2505@gmail.com>
2026-02-19 16:06:12 +0530

    Create Array.cs

---
+++ Array.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        int[] numbers = { 1, 2, 3, 4, 5 };
+
+foreach (int n in numbers)
+{
+    Console.WriteLine(n);
+}
+
+
+    }
+}
