300cf42306936c16ad27c91b567681e48e356b92
141Taskstalker <mananthaker2505@gmail.com>
2026-02-19 16:01:24 +0530

    Create LargeNumber.cs

---
+++ LargeNumber.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Program
+{
+    static void Main() {
+
+      int a = 5;
+
+      int b = 8; 
+      
+       if (a > b) {
+          Console.WriteLine("A is greater");
+       }
+      else {
+          Console.WriteLine("B is greater");
+      }
+    }
+}
